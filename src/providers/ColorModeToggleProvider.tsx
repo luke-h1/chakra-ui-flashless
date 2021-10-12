e6e750c61e2edaabb9814f7e26a5a0effda8f66e
luke-h1 <luke.howsam@yahoo.com>
2021-10-12 18:06:23 +0100

    init

---
+++ src/providers/ColorModeToggleProvider.tsx
@@ -0,0 +1,73 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from 'react';
+
+import { ColorModeContextValue, ColorModeToggleProps } from '../types';
+import {
+  createDefaultVariables,
+  getColorValue,
+  toggleColorVariables
+} from '../helpers';
+import { usePrefersColorScheme } from '../hooks/usePrefersColorScheme';
+import { ColorMode } from '@chakra-ui/color-mode';
+
+const ColorModeContext = createContext({} as ColorModeContextValue);
+
+export function ColorModeToggleProvider({
+  theme,
+  children,
+  customVariables,
+  initialColorMode
+}: ColorModeToggleProps): JSX.Element {
+  const [colorMode, setColorMode] = useState<ColorMode>(initialColorMode);
+  const {prefersColorScheme, hasMounted} = usePrefersColorScheme();
+
+  const toggleColorMode = useCallback(() => {
+    setColorMode(prev => {
+      const newColorMode = prev === 'dark' ? 'light' : 'dark';
+
+      return newColorMode;
+    });
+  }, [colorMode, prefersColorScheme]);
+
+  useEffect(() => {
+    const shouldUsePrefersColorScheme =
+      hasMounted && !initialColorMode && prefersColorScheme;
+
+    if (!shouldUsePrefersColorScheme) {
+      return;
+    }
+
+    setColorMode(prefersColorScheme);
+  }, [hasMounted, initialColorMode, prefersColorScheme]);
+
+  useEffect(() => {
+    if (!colorMode) {
+      return;
+    }
+
+    toggleColorVariables({theme, colorMode, customVariables});
+  }, [theme, customVariables, colorMode]);
+
+  const payload = useMemo(
+    () => ({
+      colorMode,
+      toggleColorMode
+    }),
+    [colorMode, toggleColorMode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={payload}>
+      {children}
+    </ColorModeContext.Provider>
+  );
+}
+
+export const useColorMode = (): ColorModeContextValue =>
+  useContext(ColorModeContext);
